ca39c532be85b59cebb8976144e5f8a06260fcf3
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-01-19 17:09:28 +0100

    Redirect to RSS feed at site root.

--- src/newspeak/urls.py
+++ src/newspeak/urls.py
@@ -9,6 +9,13 @@ urlpatterns = patterns('',
     # surl(r'^$', SomeView.as_view(),
     #     name='newspeak_home'
     # ),)
+
+    # Static redirect to the RSS feed, until we have a 
+    # page to show here.
+    (r'^$', 'django.views.generic.simple.redirect_to',
+        {'url': '/all/rss/', 'permanent': False}
+    ),
+
     url(r'^all/rss/$', NewspeakRSSFeed(), name='rss_all'),
     url(r'^all/atom/$', NewspeakAtomFeed(), name='atom_all'),
 )
