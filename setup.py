69d5a7b9edd480fb0c94b1a93193abe95073257e
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2013-04-02 16:15:47 +0200

    We're not alpha anymore!

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     url='http://github.com/bitsoffreedom/newspeak/',
     packages=find_packages(),
     include_package_data=True,
-    classifiers=['Development Status :: 3 - Alpha',
+    classifiers=['Development Status :: 4 - Beta',
                  'Environment :: Web Environment',
                  'Framework :: Django',
                  'Intended Audience :: Developers',
